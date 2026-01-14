120a929f4c3e2f0ed1033a92a30e2ae725b180bd
Pedro Linhares <plinfps@gmail.com>
2026-01-14 20:16:21 -0300

    add groupAnagrams resolution

---
+++ Questions/GroupAnagrams/groupAnagrams.java
@@ -0,0 +1,19 @@
+package Questions.GroupAnagrams;
+
+import java.util.*;
+
+public class groupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedS = new String(charArray);
+
+            res.putIfAbsent(sortedS, new ArrayList<>());
+            res.get(sortedS).add(s);
+        }
+        return new ArrayList<>();
+    }
+}
